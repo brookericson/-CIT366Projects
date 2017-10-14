01b679aba8296d4907197597969236508df02cca
brookericson <eri11006@byui.edu>
2017-10-14 15:31:58 -0600

    week 5 assignment

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Document} from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  @Input() documents: Document[] = [
+    new Document(
+      '1001',
+      'CIT 366 Review of Basic JavaScript',
+      'Directions for Assignment 1',
+      'https://content.byui.edu/file/f0594919-9524-47eb-9f4d-5c7239c3c002/1/Lesson%201%20Assignment.pdf'
+    ),
+    new Document(
+      '1002',
+      'CIT 366 Creating an Angular Project and Components',
+      'Directions for Assignment 2',
+      'https://content.byui.edu/' +
+      'file/c67e59fd-990c-4adc-9232-8027f847c8b9/1/' +
+      'Lesson%202%20-%20Create%20CMS%20project%20and%20Contact%20components.pdf'
+    ),
+    new Document(
+      '1003',
+      'CIT 366 Components and Data Binding',
+      'Directions for Assignment 3',
+      'https://content.byui.edu/file/aa9b6af5-b882-48f5-8321-caca980e5ec9/1/Lesson%203%20Components%20and%20Databinding.pdf'
+    )
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
